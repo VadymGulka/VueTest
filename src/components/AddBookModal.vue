b135ab2f89263860b00ca227ebf2727cc3032209
VadymGulka <vgulka01@educantabria.es>
2021-06-16 08:34:58 +0200

    c

--- src/components/AddBookModal.vue
+++ src/components/AddBookModal.vue
@@ -27,7 +27,7 @@
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                     <button type="button" @click="AddBookDB" class="btn btn-primary" data-bs-dismiss="modal">
-                        Save changes
+                        Save changes1
                     </button>
                 </div>
             </div>
